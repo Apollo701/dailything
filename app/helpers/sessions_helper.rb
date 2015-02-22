877fbce6bffad31c5807bd3f74f38b952e69bfdb
Jason Dorn <jdorn@mail.sfsu.edu>
2015-02-22 00:14:57 -0800

    Added remember me to sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,6 +30,13 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
     forget(current_user)
     session.delete(:user_id)
