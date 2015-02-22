2e3c707ea4c00be4478d85a85e0254eb7f44ad9d
Jason Dorn <jdorn@mail.sfsu.edu>
2015-02-22 00:22:12 -0800

    Fixed logout bug added sessions_helper#forget and log_out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,17 +12,17 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # Returns the current logged-in user (if any).
+  # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated? (cookies[:remember_token])
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
-    end    
+    end
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -30,16 +30,17 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  # Forgets a persistent session.
+    # Forgets a persistent session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
 
+  # Logs out the current user.
   def log_out
     forget(current_user)
     session.delete(:user_id)
-    @curren_user = nil
+    @current_user = nil
   end
 end
