1417ac12375e06da8f0a157bbf939b3ea0843877
Jason Dorn <jdorn@mail.sfsu.edu>
2015-02-22 16:53:43 -0800

    Fixed typo in edit user test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
   
   def setup
-  	@user = users(:micahel)
+  	@user = users(:michael)
   end
 
   test "unsuccessful edit" do
