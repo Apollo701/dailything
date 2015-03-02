cb41dcae213b73e93315e97382de45264c09f8a7
Jason Dorn <jdorn@mail.sfsu.edu>
2015-03-01 16:15:52 -0800

    Fix yml file, add User#logged_in_user/correct_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
   	@user = User.find(params[:id])
@@ -29,7 +30,6 @@ class UsersController < ApplicationController
     if @user.update_attributes(user_params)
       flash[:success] = "Profile Updated"
       redirect_to @user
-      # Handle a successful update.
     else
       render 'edit'
     end
@@ -37,18 +37,23 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-  	params.require(:user).permit(:name, :email, :password,
-  															 :password_confirmation)
-  end
-
-  # Before filters
-
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_url
+    def user_params
+    	params.require(:user).permit(:name, :email, :password,
+    															 :password_confirmation)
     end
-  end
+
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end
 end
