7a3fd30f28f56bfb745545f202c48af307235946
Jason Dorn <jdorn@mail.sfsu.edu>
2015-03-01 23:07:44 -0800

    Add User#create reset digest and send password reset email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save 	:downcase_email
 	before_create :create_activation_digest
 	validates 		:name,	presence: true, length: { maximum: 50 }
@@ -10,13 +10,27 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, length: { minimum: 6 }, allow_blank: true
 
+	# Activates an account
 	def activate
 		update_attribute(:activated, 		true)
 		update_attribute(:activated_at, Time.zone.now)
 	end
 
+	# Sends activation email.
 	def send_activation_email
 		UserMailer.account_activation(self).deliver_now
+	end
+
+	# Sets the password reset attributes
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_token, 	 User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+
+	# Sends password reset email.
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
 	end
 
 	# Returns the hash digest of the given string.
