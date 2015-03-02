cba29979e35c6f013c7a97463d8aa2fbfcaf07bc
Jason Dorn <jdorn@mail.sfsu.edu>
2015-03-01 18:10:24 -0800

    Finish user integration test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,8 +7,8 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test "index including pagination" do
-    log_in_as(@user)
+  test "index including pagination and delete links" do
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
