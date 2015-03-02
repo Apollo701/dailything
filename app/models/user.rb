f95c3118a0b727ab9e13afc33a974f2143e9857a
Jason Dorn <jdorn@mail.sfsu.edu>
2015-03-01 23:46:28 -0800

    Add password reset expired to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,7 @@ class User < ActiveRecord::Base
 		UserMailer.password_reset(self).deliver_now
 	end
 
+
 	# Returns the hash digest of the given string.
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -60,6 +61,11 @@ class User < ActiveRecord::Base
 	# Forgets a user
 	def forget
 		update_attribute(:remember_digest, nil)
+	end
+
+	# Returns true if a password reset has expired.
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
 	end
 
 	private
